e1ba75771600ec5343ee5116d7f99728d564e213
aadil10i <aadil3003@outlook.com>
2024-11-21 22:03:22 +0400

    last if boolean conditional

---
+++ test11comparison/booleanlogic/test4/main.py
@@ -0,0 +1,10 @@
+def has_enough_energy(energy_available, distance_one_way, meters_per_energy):
+    total_distance = distance_one_way * 2
+    energy_needed =  total_distance / meters_per_energy
+    if (energy_available >= energy_needed):
+        return True
+    else: 
+        return False
+
+test = has_enough_energy(2, 1, 1)
+print(test)
