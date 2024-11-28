bd2e7c66fa8ee675a3dfda63216d60e0ab87be88
aadil10i <aadil3003@outlook.com>
2024-11-28 20:00:50 +0400

    filter messages

--- test13lists/c10/main.py
+++ test13lists/c10/main.py
@@ -1,4 +1,23 @@
-def reverse_array(items):
-    return items[-1::-1]
-    
+def filter_messages(messages):
+    filtered_message = []
+    words_removed_count = []
 
+    for message in messages:
+        words = message.split()
+        non_bad_words = []
+        words_removed = 0
+        
+        for word in words:
+            if word == "dang":
+                words_removed += 1
+            else:
+                non_bad_words.append(word)
+            
+        clean_message = " ".join(non_bad_words)
+        filtered_message.append(clean_message)
+        words_removed_count.append(words_removed) 
+
+    return filtered_message, words_removed_count
+    
+the_messages = ["darn it", "this dang thing won't work", "lets fight one on one"]
+filter_messages(the_messages)
